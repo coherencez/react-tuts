f1fd63ee5605d4f14d3c7df14089cdc4855e0f78
Thomas Swann <tkswann@mac.com>
2016-09-19 22:21:33 -0500

    basic comment box loading with dynamic data

---
+++ react-101/react101.js
@@ -0,0 +1,21 @@
+
+// var CommentList = React.createClass({
+//   render: function() {
+    
+//   }
+// });
+
+// tutorial1.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
